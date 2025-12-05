7b5223330baccd7f1a68260b3d038d79c024c9b5
arshia <mohammadali.ghiamii@gmail.com>
2025-12-06 01:47:58 +0330

    ?

--- src/app/[locale]/general/payment-plans/page.tsx
+++ src/app/[locale]/general/payment-plans/page.tsx
@@ -1,6 +1,6 @@
 import { BreadCrumbFa } from "@/components/shared/breadCrumb/breadCrumbFa";
 import { Container, Stack } from "@mui/material";
-import { HeroSection } from "./_components";
+import { HeroSection, StepByStepSection } from "./_components";
 import { CommonQuestions, ExtraFooterInformation } from "@/components/shared/_components";
 
 export default function PaymentPlansPage() {
@@ -14,6 +14,11 @@ export default function PaymentPlansPage() {
                 <Stack className="items-center! justify-center! w-full! h-full!">
                     <HeroSection />
                 </Stack>
+
+                <Stack mt={{ xs: 4, md: 8 }} className="justify-center! items-center!">
+                    <StepByStepSection />
+                </Stack>
+
             </Container>
             <Container maxWidth="xl" >
                 <Stack className="hidden! md:block!" mt={{ xs: 6, md: 8 }}>
